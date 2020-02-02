dff4fdcfd4fc1d2955ead0b4e74c91af9ca7f0c4
Phong <phongdoan.dev@gmail.com>
2020-02-02 15:06:31 -0800

    skeleton project completed

---
+++ src/main/java/com/GotYourBac/gotYourBac/models/DrinkRepository.java
@@ -0,0 +1,6 @@
+package com.GotYourBac.gotYourBac.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrinkRepository extends JpaRepository<Drink, Long> {
+}
