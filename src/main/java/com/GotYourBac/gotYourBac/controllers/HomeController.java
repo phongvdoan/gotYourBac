475d5ecbe98e41c2fb791f03ff16e8ffc2bcf672
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2020-02-03 14:55:19 -0800

    added fragments to header, nav and footer

--- src/main/java/com/GotYourBac/gotYourBac/controllers/HomeController.java
+++ src/main/java/com/GotYourBac/gotYourBac/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.GotYourBac.gotYourBac.controller;
+package com.GotYourBac.gotYourBac.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping
-    public String gethome(){
+    @GetMapping("/")
+    public String getHome(){
         return "home";
     }
 }
