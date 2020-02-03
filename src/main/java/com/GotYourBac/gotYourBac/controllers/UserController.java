ff3df04b37e1290c76526997e43189c68a490e60
Phong <phongdoan.dev@gmail.com>
2020-02-03 11:16:27 -0800

    set up integration testing and added one test

--- src/main/java/com/GotYourBac/gotYourBac/controllers/UserController.java
+++ src/main/java/com/GotYourBac/gotYourBac/controllers/UserController.java
@@ -15,19 +15,23 @@ public class UserController {
     @Autowired
     ApplicationUserRepository applicationUserRepository;
 
+    //autowired passwordEncoder to use BCrypt for unique password.
+    //TODO: Lets look at having a minimum password requirement.
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    //go to registration page.
     @GetMapping("/registration")
     public String getRegistration(){
         return "registration";
     }
 
+    //Create a new user in DB, ensure we don't user Postgresql pre-defined variables. i.e 'user'
     @PostMapping("/registration")
     public RedirectView createNewUser(String username, String password, String firstName, String lastName, String gender, double height, float weight) {
         ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password), firstName, lastName, gender, height, weight);
         applicationUserRepository.save(newUser);
-        //
+        //TODO: change the redirect route to what we decide. For now its to the homepagegit
         return new RedirectView("/");
     }
 }
--- src/test/java/com/GotYourBac/gotYourBac/GotYourBacApplicationTests.java
+++ src/test/java/com/GotYourBac/gotYourBac/GotYourBacApplicationTests.java
@@ -1,13 +1,43 @@
 package com.GotYourBac.gotYourBac;
 
+import com.GotYourBac.gotYourBac.controllers.UserController;
+import com.GotYourBac.gotYourBac.models.ApplicationUserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GotYourBacApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	UserController userController;
 
 	@Test
 	void contextLoads() {
+//		assertThat();
 	}
+
+	//Test homepage rendering
+	@Test
+	public void shouldHaveHomePage() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("<h2>Make new users here</h2>"));
+	}
+
+
 
 }
