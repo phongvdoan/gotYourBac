2434edf377f86eae6352c67fafc655b8bd7bbd85
Phong <phongdoan.dev@gmail.com>
2020-02-07 00:07:21 -0800

    fixed no closing tags

--- src/main/java/com/GotYourBac/gotYourBac/controllers/AppErrorController.java
+++ src/main/java/com/GotYourBac/gotYourBac/controllers/AppErrorController.java
@@ -2,20 +2,18 @@ package com.GotYourBac.gotYourBac.controllers;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-//
+
+
 //https://www.baeldung.com/spring-boot-custom-error-page
 
     @Controller
     public class AppErrorController implements ErrorController {
 
         @RequestMapping("/error")
-        public String handleError(HttpServletRequest request, Model model) {
-//
+        public String handleError() {
             return "error";
         }
 
